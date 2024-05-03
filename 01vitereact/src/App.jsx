70d4583a0063f74822b53193c8e718834cfa550e
Harshil Patel <harshilp046@gmail.com>
2024-05-03 23:16:48 +0530

    day-01 ReactJS + Vite
    project structure and creation techniques

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>this is react W/ Vite</h1>
+  );
+}
+
+export default App
---
+++ myapp/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>this is react | W/ Harshil</h1>
+  );
+}
+
+export default App;
