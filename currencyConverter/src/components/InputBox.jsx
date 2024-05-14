64f71e445ea0601d0ae401ea310549fc32c12b57
Harshil Patel <harshilp046@gmail.com>
2024-05-14 20:58:28 +0530

    UI improvements, added background image and removed app.css (no use)

--- currencyConverter/src/components/InputBox.jsx
+++ currencyConverter/src/components/InputBox.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
-import React from "react";
+import React, { useId } from "react";
 
 function InputBox({
   label,
@@ -15,11 +15,19 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }) {
+  const amountInputId = useId();
+
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">label</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
