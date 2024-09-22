4c30302905955bf24f89a492ea33f87a74756f27
Harshil Patel <harshilp046@gmail.com>
2024-09-23 00:54:38 +0530

    updated styling

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,16 +1,19 @@
-import './App.css'
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-
   return (
     <>
-      <h1>Learning about redux toolkit</h1>
-      <AddTodo />
-      <Todos />     
+      <h1 className="text-center m-28 text-4xl font-bold">
+        TO-DO App using RTK
+      </h1>
+
+      <div className="bg-[#593d88] flex flex-col rounded-lg ml-96 mr-96 p-5">
+        <AddTodo />
+        <Todos />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -17,10 +17,10 @@ function AddTodo() {
   }
 
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+    <form onSubmit={addTodoHandler} className="space-x-3 p-3 text-center">
       <input
         type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-white text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
