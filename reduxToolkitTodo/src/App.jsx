8a7a52635388e42a834471c266c029221831ee19
Harshil Patel <harshilp046@gmail.com>
2024-09-24 11:58:00 +0530

    UI changes

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -4,11 +4,12 @@ import Todos from "./components/Todos";
 function App() {
   return (
     <>
-      <h1 className="text-center m-28 text-4xl font-bold">
-        TO-DO App using RTK
+      <h1 className="text-center text-5xl font-bold bg-violet-500 block m-auto pt-10 pb-10">
+        {" "}
+        TO-DO <span className="italic">Redux</span>
       </h1>
 
-      <div className="bg-[#593d88] rounded-lg w-1/2 p-5 fixed left-1/4 top-1/3 translate-">
+      <div className="mt-10 text-center font-semibold bg-violet-500 w-2/4 block m-auto pt-6 pb-6 rounded-md">
         <AddTodo />
         <Todos />
       </div>
--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -18,16 +18,17 @@ function AddTodo() {
 
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 p-3 text-center">
-      <input
-        type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-white text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Todo..."
+      <input 
+        type='text'
+        className='bg-gray-900 outline-none p-3 text-white rounded-lg w-2/3 focus:outline-white'
+        placeholder='Enter a Todo...'
         value={input}
         onChange={(e) => setInput(e.target.value)}
-      />
+        />
       <button
-        type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        type='submit'
+        className='ml-5 bg-gray-900 p-3 duration-200 outline-none border-2 border-gray-900 rounded-[5rem] text-white hover:border-solid hover:border-2 hover:border-white focus:outline-white'
+
       >
         Add Todo
       </button>
